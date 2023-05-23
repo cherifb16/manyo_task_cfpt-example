beebc32c6bf4123dd28042fc84890c96a67ef83c
cherifb16 <cherifbobbyray@gmail.com>
2023-05-23 14:17:12 +0200

    test

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'kaminari'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
@@ -34,7 +34,6 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'rexml'
   gem 'factory_bot_rails'
-  gem 'kaminari'
 end
 
 group :development do
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,35 +6,36 @@ RSpec.describe 'Task management function', type: :system do
       it 'The registered task is displayed' do
         visit new_task_path
         fill_in "Titre", with: "title"
-        fill_in "Content", with: "content"
-        click_button "Create Task"
-        expect(page).to have_content "Task was successfully created."
+        fill_in "contenu", with: "content"
+        click_button "Creer"
+        expect(page).to have_content "J'ai enregistré une tâche"
       end
     end
   end
 
   describe 'List display function' do
-      let!(:task){ FactoryBot.create(:task, title:'first_task')}
-      let!(:task){ FactoryBot.create(:task, title:'second_task')}
-      let!(:task){ FactoryBot.create(:task, title:'third_task')}
+    let!(:first_task) { FactoryBot.create(:task, titre: "first_task", created_at: Time.zone.now.ago(3.days)) }
+    let!(:second_task) { FactoryBot.create(:task, titre: "second_task", created_at: Time.zone.now.ago(2.days)) }
+    let!(:third_task) { FactoryBot.create(:task, titre: "third_task", created_at: Time.zone.now.ago(1.days)) }
       before do
         visit tasks_path
       end
 
       context 'When transitioning to the list screen' do
        it 'The list of created tasks is displayed in descending order of creation date and time.' do
-        FactoryBot.create(:task)
-        visit tasks_path
-        expect(page).to have_content 'Test1'
+        task_list = all('body tbody tr')
+        expect(task_list[0]).to have_content 'third_task'
+        expect(task_list[1]).to have_content 'second_task'
+        expect(task_list[2]).to have_content 'first_task'
       end
     end
       context 'When creating a new task' do
         it 'New task is displayed at the top' do
         visit new_task_path
-        fill_in "Titre", with: "title"
-        fill_in "Content", with: "content"
-        click_button "Create Task"
-        expect(all('tbody tr')[0].text).to have_content Task.last.title 
+        fill_in "Titre", with: "title1"
+        fill_in "contenu", with: "content"
+        click_button "Creer"
+        expect(page).to have_content "title1"
         end
       end
     end
@@ -42,15 +43,12 @@ RSpec.describe 'Task management function', type: :system do
   describe 'Detailed display function' do
         
      context 'When transitioned to any task details screen' do
+        let (:task) {FactoryBot.create(:task, titre: 'Test', content: 'Je suis un contenu')}
         it 'The content of the task is displayed' do
          
           visit task_path(task)
-          all('tbody tr')[0].click_button"show-task"
-            expect(page).to have_content 'Test2'
-            expect(page).to have_content 'Test valide encore'
-            expect(page).to have_content first_task.set_date
-            expect(page).to have_button'éditer'
-            expect(page).to have_button'retour'
+          visit task_path(task)
+          expect(page).to have_content 'Je suis un contenu'
         end
      end
   end
