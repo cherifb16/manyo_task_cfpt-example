065732a0d7acbdabe1d26b19dfc90a9afe80f034
cherifb16 <cherifbobbyray@gmail.com>
2023-05-24 14:49:04 +0200

    adding columns deadline,status,priority

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:titre, :content)
+      params.require(:task).permit(:titre, :content, :deadline_on, :priority, :status)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   validates :titre, :content, presence: true
+  validates :deadline_on, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+
+  enum priority: { low: 0, middle: 1, high: 2 }
+  enum status: { todo: 0, doing: 1, done: 2 }
 end
