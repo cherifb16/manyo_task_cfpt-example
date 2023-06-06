c60b281c1665028791b151b38900111d31d47705
cherifb16 <cherifbobbyray@gmail.com>
2023-06-06 14:02:11 +0200

    adding users crud and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+  include SessionsHelper
+
+  before_action :login_required
+
+  private
+    def login_required
+      redirect_to new_session_path, alert: "Please log in." unless current_user
+    end
+
+    def already_logged_in
+      redirect_to tasks_path, alert: "Please log in." if current_user
+    end
 end
